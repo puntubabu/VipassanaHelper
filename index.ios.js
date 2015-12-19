680709a50ca707312f6bb57b017d87690e5e5e0f
Pradeep Keshary <puntubabu@gmail.com>
2015-12-19 12:31:57 -0500

    styling changes, added image of dhamma wheel

--- index.ios.js
+++ index.ios.js
@@ -7,6 +7,18 @@
 var React = require('react-native');
 var TimerMixin = require('react-timer-mixin');
 var moment = require('moment');
+var AudioPlayer = require('react-native-audioplayer');
+
+//Constants
+var START_TEXT = 'Start';
+var STOP_TEXT = 'Stop';
+var RESET_TEXT = 'Reset';
+var AUDIO_0 = '0.mp3';
+var AUDIO_1 = '1.mp3';
+var AUDIO_2 = '2.mp3';
+var PLAY_AUDIO_1 = 2700000;
+var PLAY_AUDIO_2 = 3540000;
+var PADDING = 2000;
 
 var {
   AppRegistry,
@@ -26,6 +38,7 @@ var VipassanaTimerApp = React.createClass({
       timeDiff: null,
       startStopText: "Start",
       hasStarted: false,
+      isPlayingAudio: false,
     };
   },
 
@@ -34,8 +47,46 @@ var VipassanaTimerApp = React.createClass({
   },
 
   handleTick: function() {
+    var that = this;
     var timeDiff = new Date().getTime() - this.state.startTime;
     this.setState({ timeDiff: timeDiff });
+
+    //If anechya or bhavatu
+    if (timeDiff >= PLAY_AUDIO_1 && timeDiff < (PLAY_AUDIO_1 + PADDING)) {
+      AudioPlayer.play(AUDIO_1);
+    } else if (timeDiff >= PLAY_AUDIO_2 && timeDiff < (PLAY_AUDIO_2 + PADDING)) {
+      AudioPlayer.play(AUDIO_2);
+      that.stop();
+    }
+  },
+
+  start: function() {
+    var that = this;
+
+    //Start Intro Audio
+    AudioPlayer.play(AUDIO_0);
+
+    this.setState({ 
+      startTime: new Date().getTime(),
+      startStopText: STOP_TEXT,
+      hasStarted: true,
+    });
+
+    this.intervalFunction = this.setInterval(
+      () => {
+        that.handleTick();
+      }, 1000
+    );
+  },
+
+  stop: function() {
+
+    this.setState({ 
+      hasStarted: false, 
+      startStopText: START_TEXT
+    });
+
+    clearInterval(this.intervalFunction);
   },
 
   startOrStop: function() {
@@ -43,26 +94,16 @@ var VipassanaTimerApp = React.createClass({
     var that = this;
     var startOrStopText;
 
-    if (this.state.hasStarted) {
+    if (this.state.hasStarted)
+      this.stop();
+    else
+      this.start();
 
-      startOrStopText = "Stop";
-      this.setState({ hasStarted: false, });
+  },
 
-    } else {
-
-      startOrStopText = "Start";
-      this.setState({ 
-        startTime: new Date().getTime(),
-        startStopText: startOrStopText,
-        hasStarted: true,
-      });
-      this.intervalFunction = this.setInterval(
-        () => {
-          that.handleTick();
-        }, 1000
-      );
-    }
-
+  reset: function() {
+    clearInterval(this.intervalFunction);
+    this.setState({ timeDiff: 0, startStopText: START_TEXT });
   },
 
   intervalFunction: function() { },
@@ -76,32 +117,68 @@ var VipassanaTimerApp = React.createClass({
     var elapsedTime = this.state.timeDiff ? moment.utc(this.state.timeDiff).format("HH:mm:ss").toString() : "00:00:00";
 
     return (
-      <View style={styles.container}>
-        <Text>{elapsedTime}</Text>
-        <View style={styles.container}>
+      <View style={styles.outerContainer}>
+        <View style={styles.centerItem}>
+          <Image style={styles.backgroundImage} source={require('./dharma_wheel.png')} />
+        </View>
+        <View style={styles.centerItem}>
+          <Text style={styles.elapsedTime}>{elapsedTime}</Text>
+        </View>
+        <View style={styles.innerContainer}>
           <TouchableHighlight onPress={this.startOrStop} style={styles.btn}>
-            <Text>{this.state.startStopText}</Text>
+            <Text style={styles.startStopText}>{this.state.startStopText}</Text>
+          </TouchableHighlight>
+          <TouchableHighlight onPress={this.reset} style={styles.btn}>
+            <Text style={styles.startStopText}>Reset</Text>
           </TouchableHighlight>
         </View>
       </View>
+
+
     );
   },
 
 });
 
 var styles = StyleSheet.create({
-  container: {
-    flex: 1,
+  outerContainer: {
+    backgroundColor: '#F2F2F2',
+    marginTop: 25,
+    flexDirection: 'column',
+    flex:1,
+  },
+  innerContainer: {
+    backgroundColor: '#F2F2F2',
+    flex:1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginLeft: 40,
+    marginRight: 40,
+  },
+  centerItem: {
+    flex:1,
     flexDirection: 'row',
     justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  rightContainer: {
-    flex: 1,
   },
   btn: {
     backgroundColor: '#F00',
+    borderRadius: 3,
+    borderStyle: 'solid',
+    borderWidth: 2,
+    borderColor: '#000'
+  },
+  backgroundImage: {
+    height:180,
+    width:180,
+  },
+  startStopText: {
+    fontSize: 28,
+    alignItems: 'center',
+  },
+  elapsedTime: {
+    fontSize: 42,
+    marginTop: 50,
   },
 });
 
